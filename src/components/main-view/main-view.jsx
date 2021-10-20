3908b6e6164199d0829ce6fe93fa7c306ebbb0f0
DaniLott1988 <danilott1988@gmail.com>
2021-10-20 14:32:11 +0200

    Task 3.3 V02
    Added the needed changes to make the "Back" button and the posters work.

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import axios from 'axios';
+
 import { MovieCard } from '../movie-card/movie-card';
 import { MovieView } from '../movie-view/movie-view';
 
@@ -8,9 +10,10 @@ export class MainView extends React.Component {
     super();
     this.state = {
       movies: [
-        {_id: 1, Title: "TLotR: The Fellowship of the Ring", Description: "A meek Hobbit from the Shire and eight companions set out on a journey to destroy the powerful One Ring and save Middle-earth from the Dark Lord Sauron.", ImagePath: "img/lord01.jpg" },
-        {_id: 2, Title: "TLotR: The Two Towers", Description: "The continuing quest of Frodo and the Fellowship to destroy the One Ring. Frodo and Sam discover they are being followed by the mysterious Gollum. Aragorn, the Elf archer Legolas, and Gimli the Dwarf encounter the besieged Rohan kingdom, whose once great King Theoden has fallen under Saruman's deadly spell.", ImagePath: "https://upload.wikimedia.org/wikipedia/en/d/d0/Lord_of_the_Rings_-_The_Two_Towers_%282002%29.jpg"},
-        {_id: 3, Title: "TLotR: The Return of the King", Description: "The final confrontation between the forces of good and evil fighting for control of the future of Middle-earth. Frodo and Sam reach Mordor in their quest to destroy the One Ring, while Aragorn leads the forces of good against Sauron's evil army at the stone city of Minas Tirith.", ImagePath: "https://static.wikia.nocookie.net/lotr/images/b/ba/Rie.jpg/revision/latest/scale-to-width-down/334?cb=20210717110214"},
+        {_id: 1, Title: "TLotR: The Fellowship of the Ring", Description: "A meek Hobbit from the Shire and eight companions set out on a journey to destroy the powerful One Ring and save Middle-earth from the Dark Lord Sauron.", ImagePath: require("../main-view/img/lord01.jpg")
+         },
+        {_id: 2, Title: "TLotR: The Two Towers", Description: "The continuing quest of Frodo and the Fellowship to destroy the One Ring. Frodo and Sam discover they are being followed by the mysterious Gollum. Aragorn, the Elf archer Legolas, and Gimli the Dwarf encounter the besieged Rohan kingdom, whose once great King Theoden has fallen under Saruman's deadly spell.", ImagePath: require("../main-view/img/lord02.jpg") },
+        {_id: 3, Title: "TLotR: The Return of the King", Description: "The final confrontation between the forces of good and evil fighting for control of the future of Middle-earth. Frodo and Sam reach Mordor in their quest to destroy the One Ring, while Aragorn leads the forces of good against Sauron's evil army at the stone city of Minas Tirith.", ImagePath: require("../main-view/img/lord03.jpg") },
       ],
       selectedMovie: null
     };
@@ -23,8 +26,6 @@ export class MainView extends React.Component {
   
   render() {
     const { movies, selectedMovie } = this.state;
-
-    if (selectedMovie) return <MovieView movie={selectedMovie} />;
 
     if (movies.lenght === 0) return <div className="main-view">The list is empty!</div>;
 
