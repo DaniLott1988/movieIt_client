b31f227165d57ca83bf8177976cde8be59266e23
DaniLott1988 <danilott1988@gmail.com>
2022-03-07 19:00:06 +0100

    Update 01

--- src/index.jsx
+++ src/index.jsx
@@ -1,17 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Container from 'react-bootstrap/Container';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import { devToolsEnhancer } from 'redux-devtools-extension';
+
+import moviesApp from './reducers/reducers';
 import MainView from './components/main-view/main-view';
 
 import './index.scss';
 
+const store = createStore(moviesApp, devToolsEnhancer());
+
 class MyFlixApp extends React.Component {
+
   render() {
+
     return(
-      <MainView />
+    
+      <Provider store={store}>
+        <Container>
+          <MainView />
+        </Container>
+      </Provider>
     );
   }
 }
 
-const container = document.getElementsByClassName('app-container') [0];
+const container = document.getElementsByClassName('app-container')[0];
 
 ReactDOM.render(React.createElement(MyFlixApp), container);
